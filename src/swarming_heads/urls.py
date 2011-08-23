fc8864eee378a1604fbcd352ebd79e95d1853a33
Daniel Grech <danielgrech91@gmail.com>
2011-08-23 21:06:24 +1000

    Added index page to root address

--- src/swarming_heads/urls.py
+++ src/swarming_heads/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
 admin.autodiscover()
-
 
 # See https://docs.djangoproject.com/en/dev/intro/tutorial03/#design-your-urls
 # to understand what is going on here
@@ -18,4 +15,7 @@ urlpatterns = patterns('',
     # Urls to handle admin work
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    
+    #Url to catch the homepage
+    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'index.html'}),
 )
